52acab8d7fbf250faefce4a4809aa4b366f93c34
Keefer Taylor <18271723+keefertaylor@users.noreply.github.com>
2021-11-21 14:17:27 -0800

    Add some types (#2)
    Co-authored-by: Keefer Taylor <keefer@keefertaylor.com>

---
+++ core/src/types/tezos-transaction.ts
@@ -0,0 +1,7 @@
+/** 
+ * Represents a Tezos transaction object encoded in JSON 
+ *
+ * TODO(keefertaylor): This definition is pretty loose now, we can refine it for better typing in 
+ *                     code later.
+ */
+export type TezosTransaction = Object
