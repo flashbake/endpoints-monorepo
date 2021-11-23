2dd1f700ee999985779c5c2b737485038dfe933b
Keefer Taylor <keefer@keefertaylor.com>
2021-11-22 21:25:09 -0800

    Mempool abstraction and in-memory implementation

--- core/src/index.ts
+++ core/src/index.ts
@@ -6,3 +6,6 @@
 export { Bundle } from './types/bundle'
 export { Address, TransactionHash } from './types/core'
 export { TezosTransaction } from './types/tezos-transaction'
+
+// Classes
+export { default as BundleUtils } from './bundle-utils'
---
+++ relay/src/interfaces/mempool.ts
@@ -0,0 +1,31 @@
+import { Bundle } from '@flashbake/core'
+
+/**
+ * A pool of pending bundles to be placed in Flashback.
+ * 
+ * TODO(keefertaylor): It is unclear what identifiers should exist here. How do we do `Bundle` equality checks.
+ */
+export default interface Mempool {
+  /** 
+   * Add a `Bundle` to the pool. 
+   *
+   * @param bundle The bundle to add.
+   * @returns A boolean indicating whether the given `Bundle` was accepted. 
+   */
+  addBundle(bundle: Bundle): Promise<boolean>
+
+  /** 
+   * Retrieve a list of `Bundle`s in the pool.
+   * 
+   * @returns An array of `Bundle`s in the pool.
+   */
+  getBundles(): Promise<Array<Bundle>>
+
+  /** 
+   * Remove a `Bundle` from the pool. 
+   *
+   * @param bundle The `Bundle` to remove
+   * @returns A boolean indicating whether the `Bundle` was removed. 
+   */
+  removeBundle(bundle: Bundle): Promise<boolean>
+}
