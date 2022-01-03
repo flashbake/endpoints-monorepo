72bea099da0d3e9b0130c774b7afd240d1f66e29
Daniel Feygin <daniel.feygin@gmail.com>
2022-01-03 16:07:32 -0300

    updated exports and dependencies

--- relay/src/index.ts
+++ relay/src/index.ts
@@ -1,2 +1,18 @@
-// Stub code for CI. 
-const x = 1
+/**
+ * Export all components.
+ */
+
+// Types
+export { RegistryValue } from './types/registry-value'
+
+// Interfaces
+export { default as IndexerService } from './interfaces/indexer-service'
+export { default as Mempool } from './interfaces/mempool'
+export { RegistryService } from './interfaces/registry-service'
+export { default as RpcService } from './interfaces/rpc-service'
+
+// Classes
+export { default as StubRegistryService } from './implementations/in-memory/stub-registry-service'
+export { default as InMemoryMempool } from './implementations/in-memory/in-memory-mempool'
+export { default as InMemoryRegistryService } from './implementations/in-memory/in-memory-registry-service'
+export { default as TaquitoRpcService } from './implementations/taquito/taquito-rpc-service'
