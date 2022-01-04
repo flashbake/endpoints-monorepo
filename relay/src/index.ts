440742ba0b4f67326046e4d68d439dfb65a00c0b
Daniel Feygin <daniel.feygin@gmail.com>
2022-01-04 15:37:15 -0300

    removed dangling dependency (#9)

--- relay/src/index.ts
+++ relay/src/index.ts
@@ -12,7 +12,6 @@ export { RegistryService } from './interfaces/registry-service'
 export { default as RpcService } from './interfaces/rpc-service'
 
 // Classes
-export { default as StubRegistryService } from './implementations/in-memory/stub-registry-service'
 export { default as InMemoryMempool } from './implementations/in-memory/in-memory-mempool'
 export { default as InMemoryRegistryService } from './implementations/in-memory/in-memory-registry-service'
 export { default as TaquitoRpcService } from './implementations/taquito/taquito-rpc-service'
