6d80f40276ac1eefed9b699c99bcec969f921ff5
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-07-08 14:49:41 -0700

    Move app.ts to this repo (#19)
    * Move app.ts to this repo
    
    As we were developing the prototype, we grew these files in the
    prototype repo itself. But they turned into binaries, that should be
    installed with the npm install command. Consequently we are moving them
    here.
    
    * fix container build dockerfile
    
    * new docker container names to match npm packages
    
    * start right app
    
    * add missing dep
    
    * add binary support
    
    * hardcode command name, refuse to start if subcommand not passed

---
+++ baker-endpoint/src/app.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import express from 'express';
+import { Mempool, InMemoryMempool } from '@flashbake/relay';
+import { HttpBakerEndpoint } from '.';
+import yargs, { Argv } from "yargs";
+
+function startBakerEndpoint(relayListenerPort: number, bakerListenerPort: number, rpcApiUrl: string): HttpBakerEndpoint {
+  const relayFacingApp = express();
+  const bakerFacingApp = express();
+  const mempool: Mempool = new InMemoryMempool();
+  const baker = new HttpBakerEndpoint(relayFacingApp, bakerFacingApp, mempool, rpcApiUrl);
+
+  relayFacingApp.listen(relayListenerPort, () => {
+    console.log(`Baker Endpoint relay-facing listener started on http://localhost:${relayListenerPort}`);
+  }).setTimeout(500000);
+  bakerFacingApp.listen(bakerListenerPort, () => {
+    console.log(`Baker Endpoint baker-facing listener started on http://localhost:${bakerListenerPort}`);
+  }).setTimeout(500000);
+
+  return baker;
+}
+
+async function main() {
+  let argv = await yargs
+    .scriptName("flashbake-baker-endpoint")
+    .command('start', "Start flashbake-baker-endpoint.", (yargs: Argv) => {
+      return yargs.option('relay_listener_port', {
+        describe: "Relay listener port",
+        type: "number",
+        demandOption: true,
+      }).option('tezos_rpc_url', {
+        describe: "Tezos node RPC API URL",
+        type: "string",
+        demandOption: true,
+      }).option('baker_listener_port', {
+        describe: "Baker listener port",
+        type: "number",
+        demandOption: true,
+      })
+    }).demandCommand(1, 'You need to pass the run command, as in "flashbake-baker-endpoint run"').argv;
+
+  startBakerEndpoint(argv.relay_listener_port, argv.baker_listener_port, argv.tezos_rpc_url);
+}
+
+main();
